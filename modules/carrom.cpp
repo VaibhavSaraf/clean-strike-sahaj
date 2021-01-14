c4457fd5d8603d4d293a6e7832b20b6fba7e3304
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2021-01-14 11:21:44 +0530

    2

---
+++ modules/carrom.cpp
@@ -0,0 +1,53 @@
+class Carrom
+{
+public:
+    int black;
+    int red;
+    Carrom()
+    {
+        black = 9;
+        red = 1;
+    }
+    int strike()
+    {
+        if (black == 0)
+            return 0;
+        black--;
+        return 1;
+    }
+    int multistrick()
+    {
+        if (black == 0)
+            return 0;
+        black = black - 2;
+        return 2;
+    }
+    int redstrike()
+    {
+        if (red == 0)
+            return 0;
+
+        red--;
+        return 3;
+    }
+    int striker()
+    {
+        return -1;
+    }
+    int defunct()
+    {
+        if (black == 0)
+            return 0;
+        black--;
+        return -2;
+    }
+    int emptyStrike()
+    {
+        return 0;
+    }
+    bool coinCheck()
+    {
+        if(black==0 && red==0) return true;
+        else return false;
+    }
+};
--- run.cpp
+++ run.cpp
@@ -1,20 +1,124 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include "modules/play.cpp"
+#include "modules/carrom.cpp"
 using namespace std;
 #define OJ                            \
     freopen("input.txt", "r", stdin); \
     freopen("output.txt", "w", stdout);
-void solve()
-{
-    int i, j, n, m;
-}
+
 int main()
 {
-
-    int t = 1;
-    cin >> t;
-    while (t--)
+    string score;
+    int option;
+    int result;
+    cout << "Loading..." << endl;
+    int player = 1;
+    Carrom cleanstrikeboard;
+    Play gameplayers;
+    while (gameplayers.gamewin() != true)
     {
-        solve();
+        player = (player + 1) % 2;
+        if (((gameplayers.checkplaysc() == true) && (gameplayers.scoreddifference() >= 3)) || (cleanstrikeboard.coinCheck()))
+        {
+            score = gameplayers.gameresult();
+            cout << score << " won the game."
+                 << " Final Score: " << finalplay1sc << '-' << finalplay2sc;
+            break;
+        }
+
+        cout << endl
+             << "Player " << player + 1 << ": Choose an outcome from the list below" << endl;
+        cout << "1. Strike" << endl;
+        cout << "2. Multi Strike" << endl;
+        cout << "3. Red Strike" << endl;
+        cout << "4. Striker Strike" << endl;
+        cout << "5. Defunct Coin" << endl;
+        cout << "6. None" << endl;
+        cout << "Current Score" << endl
+             << "Player 1 Score: " << gameplayers.play1sc << endl
+             << "Player 2 Score: " << gameplayers.play2sc;
+        cout << endl
+             << "Type Your Option";
+        cin >> option;
+        if (option != 1 && option != 2 && option != 3 && option != 4 && option != 5 && option != 6)
+        {
+            cout << endl
+                 << "Wrong Option Try Again";
+            player = (player + 1) % 2;
+            continue;
+        }
+        if (cleanstrikeboard.red < 1 && option == 3)
+        {
+            cout << endl
+                 << "There are not enough red coins on the board";
+            cout << endl
+                 << "Red coin left on board are: " << cleanstrikeboard.red;
+            player = (player + 1) % 2;
+            continue;
+        }
+        if (cleanstrikeboard.black < 2 && option == 2 || cleanstrikeboard.black < 1 && option == 1 || cleanstrikeboard.black <= 0 && option == 5)
+        {
+            cout << endl
+                 << "There are not enough black coins on the board";
+            cout << endl
+                 << "Black coin left on board are: " << cleanstrikeboard.black;
+            player = (player + 1) % 2;
+            continue;
+        }
+        if (option == 1)
+        {
+            gameplayers.clearcount(player + 1);
+            result = cleanstrikeboard.strike();
+            gameplayers.updateplaysc(player + 1, result);
+            outcome = "Strike";
+            gameplayers.history(player + 1);
+        }
+        if (option == 2)
+        {
+            gameplayers.clearcount(player + 1);
+            result = cleanstrikeboard.multistrick();
+            gameplayers.updateplaysc(player + 1, result);
+            outcome = "MultiStrike";
+
+            gameplayers.history(player + 1);
+        }
+        if (option == 3)
+        {
+            gameplayers.clearcount(player + 1);
+            result = cleanstrikeboard.redstrike();
+            gameplayers.updateplaysc(player + 1, result);
+            outcome = "RedStrike";
+            gameplayers.history(player + 1);
+        }
+        if (option == 4)
+        {
+            gameplayers.clearcount(player + 1);
+            result = cleanstrikeboard.striker();
+            gameplayers.updateplaysc(player + 1, result);
+            outcome = "StrikerStrike";
+            gameplayers.history(player + 1);
+            gameplayers.playerfoulcount(player + 1);
+        }
+        if (option == 5)
+        {
+            result = cleanstrikeboard.defunct();
+            gameplayers.updateplaysc(player + 1, result);
+            outcome = "Defunct";
+            gameplayers.history(player + 1);
+            gameplayers.playerfoulcount(player + 1);
+        }
+        if (option == 6)
+        {
+            result = cleanstrikeboard.emptyStrike();
+            gameplayers.updateplaysc(player + 1, result);
+            outcome = "Empty";
+            gameplayers.playeremptymoves(player + 1);
+            gameplayers.updateplayerhistory(player + 1);
+        }
     }
+    // getch();
+    return 0;
+
     return 0;
 }
